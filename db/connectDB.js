2cb1dbcde400690ff0765336404117f6499b23e1
MuskanGupta3109 <muskangupta9224@gmail.com>
2023-02-22 23:46:47 +0530

    xyz

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,7 +1,8 @@
  const mongoose=require('mongoose')
-
+ const url = "mongodb+srv://jooligupta2000:12345jooli@cluster0.rxpznai.mongodb.net/blogWebsite?retryWrites=true&w=majority"
 const connectDB = ()=>{
-    return mongoose.connect('mongodb://127.0.0.1:27017/blogWebsite')
+    // return mongoose.connect('mongodb://127.0.0.1:27017/blogWebsite')
+    return mongoose.connect(url)
     .then(()=>{
         console.log('connected successfully!...')
     })
@@ -9,4 +10,5 @@ const connectDB = ()=>{
         console.log(err)
     })
 }
+mongoose.set('strictQuery',false);
 module.exports=connectDB;
